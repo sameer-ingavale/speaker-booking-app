e0ee5ada02e5668e13a286eb13a64007a27abbc8
Sameer Ingavale <52799932+sameer-ingavale@users.noreply.github.com>
2019-10-09 16:03:49 +0530

    test heroku

--- client/src/pages/Auth/CreateAccount.js
+++ client/src/pages/Auth/CreateAccount.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./mainAuth.css";
 
 export default class CreateAccount extends Component {
   constructor(props) {
