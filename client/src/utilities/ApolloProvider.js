a40643ddd6f226ab93cc085f4b5fc2e2cb1aba78
Sameer Ingavale <52799932+sameer-ingavale@users.noreply.github.com>
2019-10-27 17:38:07 +0530

    final hero

--- client/src/utilities/ApolloProvider.js
+++ client/src/utilities/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import { setContext } from "apollo-link-context";
 
 const httpLink = createUploadLink({
-  uri: "http://localhost:8000/"
+  uri: "https://book-a-speaker.herokuapp.com"
 });
 
 const authLink = setContext(() => {
